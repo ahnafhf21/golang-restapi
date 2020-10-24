9173aa3eb08f4db2825f1ab52132a4fa279d0b7f
ahnafhf21 <ahnafhadi@gmail.com>
2020-10-25 02:14:47 +0700

    updated code

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import(
 	"encoding/json"
 	"log"
 	"net/http"
-	// "math/rand"
-	// "strconv"
+	"math/rand"
+	"strconv"
 	"github.com/gorilla/mux"
 )
 // Book Struct (Model)
@@ -33,22 +33,59 @@ func getBooks(w http.ResponseWriter, r *http.Request){
 
 // Get Single Book
 func getBook(w http.ResponseWriter, r *http.Request){
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // get any parameters
+	// Loop through books and find with id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 // Create a new book
 func createBook(w http.ResponseWriter, r *http.Request){
-
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(1000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 // Update existing book
 func updateBook(w http.ResponseWriter, r *http.Request){
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // get any parameters
+	// Loop through books and find with id
+	for index, item := range books {
+		if item.ID == params["id"] {
+		books = append(books[:index], books[index+1:]...)	
+		var book Book
+		_ = json.NewDecoder(r.Body).Decode(&book)
+		book.ID = params["id"]
+		books = append(books, book)
+		json.NewEncoder(w).Encode(book)
+		return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 // Delete a book
 func deleteBook(w http.ResponseWriter, r *http.Request){
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // get any parameters
+	// Loop through books and find with id
+	for index, item := range books {
+		if item.ID == params["id"] {
+		books = append(books[:index], books[index+1:]...)	
+		break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func main(){
@@ -66,6 +103,6 @@ func main(){
 	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	r.HandleFunc("/api/books", createBook).Methods("POST")
 	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", getBooks).Methods("DELETE")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
